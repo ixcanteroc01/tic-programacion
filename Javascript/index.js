abf7cb1150b870bfed3be01462bf6dadaec1c3b3
ixcanteroc01 <82376462+ixcanteroc01@users.noreply.github.com>
2021-04-23 11:36:53 +0200

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,7 +2,7 @@ function comprobar(){
    var user = document.form.user.value;
    var contraseña = document.form.contraseña.value;
    if(user == "usuario" && contraseña == "1234"){
-        window.open('inicio.html');
+        window.open('inicio.html', "_self");
    } else {
         alert("Usuario o contraseña incorrectos.\nInténtelo de nuevo.");
    }
